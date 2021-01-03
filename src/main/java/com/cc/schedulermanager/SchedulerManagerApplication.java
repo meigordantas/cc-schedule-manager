082d6ec52bf8f865f8cd37575addcaed460d2e7e
meigordantas <me.igordantas@gmail.com>
2021-01-03 09:47:46 -0300

    project dependencies

---
+++ src/main/java/com/cc/schedulermanager/SchedulerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cc.schedulermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cc/schedulermanager/SchedulerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.schedulermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
